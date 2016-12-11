9d788ceb43e958bf631b215518fd28679a14e02b
Michel Grootjans <michel.grootjans@gmail.com>
2016-12-11 12:20:59 +0100

    optimized PlayerActivity

--- generator/projections/player_activity.rb
+++ generator/projections/player_activity.rb
@@ -3,31 +3,34 @@ require 'awesome_print'
 class PlayerActivity
   def initialize(events, date)
     @events = events
-    @date = date
-    @players = @events.select { |e| e.type == 'PlayerHasRegistered' }
-                  .map(&:payload)
+    @player_activity = player_activity_on(date)
   end
 
   def active_on
-    player_activity_on(@date).select{|e| e.games_played > 10}
+    @player_activity.select{|e| e.games_played > 10}
 
   end
 
   def inactive_on
-    player_activity_on(@date).select{|e| e.games_played < 1}
+    @player_activity.select{|e| e.games_played < 1}
 
   end
 
   def player_activity_on(date)
-    @players.map do |player|
+    players.map do |player|
       games_played = join_events_on(date).select { |e| e.player_id == player.player_id }
       OpenStruct.new(player: player, games_played: games_played.count)
     end
   end
 
+  def players
+    @events.select { |e| e.type == 'PlayerHasRegistered' }
+           .map(&:payload)
+  end
+
   def join_events_on(date)
-    @events.select { |e| in_range(date, e.timestamp) }
-           .select { |e| e.type == 'PlayerJoinedGame' }
+    @events.select { |e| e.type == 'PlayerJoinedGame' }
+           .select { |e| in_range(date, e.timestamp) }
            .map(&:payload)
   end
 
